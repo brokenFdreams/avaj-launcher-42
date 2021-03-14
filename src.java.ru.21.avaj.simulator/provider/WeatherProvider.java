c0d963dc6d0ed6de4e3b15549b733cbca18aa46e
brokenFdreams <brokenFdreams@protonmail.com>
2021-03-14 12:06:06 +0300

    add random

--- src.java.ru.21.avaj.simulator/provider/WeatherProvider.java
+++ src.java.ru.21.avaj.simulator/provider/WeatherProvider.java
@@ -2,9 +2,12 @@ package provider;
 
 import aircrafts.Coordinates;
 
+import java.util.Random;
+
 public class WeatherProvider {
     private static final WeatherProvider weatherProvider = new WeatherProvider();
     private static final String[] weather = {"SUN", "RAIN", "FOG", "SNOW"};
+    private static final Random random = new Random();
 
     private WeatherProvider() {
     }
@@ -16,7 +19,8 @@ public class WeatherProvider {
     public String getCurrentWeather(Coordinates coordinates) {
         int coordinatesSum = coordinates.getLongitude() +
                 coordinates.getLatitude() +
-                coordinates.getHeight();
+                coordinates.getHeight() +
+                random.nextInt(2);
         return weather[coordinatesSum % 4];
     }
 }
